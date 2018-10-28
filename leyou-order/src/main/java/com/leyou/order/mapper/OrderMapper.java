d482bf75ea7eee4824252a914020339c1b3d5173
lyj8330328 <980500807@qq.com>
2018-10-28 10:30:22 +0800

    创建订单微服务

---
+++ leyou-order/src/main/java/com/leyou/order/mapper/OrderMapper.java
@@ -0,0 +1,13 @@
+package com.leyou.order.mapper;
+
+import com.leyou.order.pojo.Order;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Author: 98050
+ * @Time: 2018-10-27 16:31
+ * @Feature:
+ */
+public interface OrderMapper extends Mapper<Order> {
+
+}
